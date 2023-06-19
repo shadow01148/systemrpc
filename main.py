e60d7c3f87806dbd48b7d8fa052ecc38f1687a5a
Robert <76457846+shadow01148@users.noreply.github.com>
2023-06-19 14:41:19 +0800

    substitute for battery if none is available + other additions

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from pypresence import Presence
 import time
 import psutil
+import platform
 
 def rpc():
-    if psutil.sensors_battery().percent is not None: 
-        battery = psutil.sensors_battery().percent 
-
-    RPC = Presence(123) # int
+    if psutil.sensors_battery() is None: 
+        battery = f"Running on: {platform.system()}, {platform.release()}"
+    else:
+        battery = f"Battery percent: {psutil.sensors_battery().percent}%"
+    RPC = Presence()
     if RPC is None:
         print("No Client ID provided. Look here:\n\nhttps://support-dev.discord.com/hc/en-us/articles/360028717192-Where-can-I-find-my-Application-Team-Server-ID-")
         return
@@ -17,11 +19,11 @@ def rpc():
         mem = round(psutil.virtual_memory().percent,1)
         RPC.update(
             state=F"CPU: {str(cpu)}%, RAM: {str(mem)}%", 
-            details=f"CPU Cores: {psutil.cpu_count()}, Battery percent: {battery}%", # to be fixed
-            large_image="image_asset",
-            small_image="small_image_asset", 
+            details=f"CPU Cores: {psutil.cpu_count()}, {battery}", 
+            large_image="img",
+            small_image="profile", 
             large_text="RPC", # Text when hovering over large_image
             buttons=[{"label": "pypresence", "url": "https://github.com/qwertyquerty/pypresence"}]) # Maximum of 2 buttons   
-        time.sleep(5) # you can remove this to go awol
+        time.sleep(5)
 
 rpc()
